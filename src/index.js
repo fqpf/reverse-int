b58111d399dd34bf8a70371a8b9fe951f3e50f23
Anastasiya Shevchenko <sheann122117@gmail.com>
2020-10-05 16:29:44 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let strN = n.toString().split('').reverse();
+    let newArr = [];
+    for (let i = 0; i < strN.length; i++) {
+        if (strN[i] !== '-') {
+            newArr.push(strN[i])
+        }
+    }
+    return newArr.join('')
+};
